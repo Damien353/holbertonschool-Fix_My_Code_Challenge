6a163bffe4347f8276040994dad3d482f254772e
Damien353 <10034@holbertonstudents.com>
2025-05-12 12:10:25 +0200

    Fixing delete dnodeint after else

---
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	current = *head;
+
+	/* Move to the node at the given index */
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+
+	/* If index is out of range */
+	if (current == NULL)
+		return (-1);
+
+	/* If we're deleting the head */
+	if (current->prev == NULL)
+	{
+		*head = current->next;
+		if (current->next != NULL)
+			current->next->prev = NULL;
+	}
+	else
+	{
+		current->prev->next = current->next;
+		if (current->next != NULL)
+			current->next->prev = current->prev;
+	}
+
+	free(current);
+	return (1);
+}
